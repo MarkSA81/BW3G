762bd3b97d266100e13901473505ba2aeb87d269
MarkSA81 <markamoras@gmail.com>
2025-11-19 09:02:41 -0300

    Update trapinch.asm

--- data/pokemon/dex_entries/trapinch.asm
+++ data/pokemon/dex_entries/trapinch.asm
@@ -1,10 +1,10 @@
-	db "ANT PIT@" ; species name
-	dw 204, 331 ; height, weight
+	db "LANDSHARK@" ; species name
+	dw 204, 452 ; height, weight
 
-	db "This #MON waits"
-	next "patiently for prey"
-	next "to fall into its"
+	db "It nests in small,"
+	next "horizontal holes"
+	next "in cave walls. It"
 
-	page "pit. Its jaws are"
-	next "strong enough to"
-	next "crush boulders.@"
+	page "pounces to catch"
+	next "prey that stray"
+	next "too close.@"
