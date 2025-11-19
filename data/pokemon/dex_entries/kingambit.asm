0adce695e7051992ec37cdee4db0a30fba3c022b
MarkSA81 <markamoras@gmail.com>
2025-11-19 08:58:09 -0300

    Update and rename corsola.asm to kingambit.asm

---
+++ data/pokemon/dex_entries/kingambit.asm
@@ -0,0 +1,10 @@
+	db "BIGBLADE@" ; species name
+	dw 607, 2646 ; height, weight
+
+	db   "Only a Bisharp "
+	next "that stands above"
+	next "all others"
+
+	page "in its vast army"
+	next "can evolve"
+	next "into Kingambit.@"
