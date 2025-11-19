4965d5d1fa27e81bfe1396e59d4ca0477d1c3e71
MarkSA81 <markamoras@gmail.com>
2025-11-19 09:13:57 -0300

    Update and rename flygon.asm to garchomp.asm

---
+++ data/pokemon/dex_entries/garchomp.asm
@@ -0,0 +1,9 @@
+	db "MACH@" ; species name
+	dw 603, 2094 ; height, weight
+
+	db "It flies at speeds"
+	next "equal to a jet"
+	next "fighter plane. It"
+
+	page "never allows its"
+	next "prey to escape.@"
