9f9ab67f11dcafed85f870022de5477b6a287544
MarkSA81 <markamoras@gmail.com>
2025-11-19 09:05:00 -0300

    Update and rename vibrava.asm to gabite.asm

---
+++ data/pokemon/dex_entries/gabite.asm
@@ -0,0 +1,10 @@
+	db "CAVE@" ; species name
+	dw 407, 1235 ; height, weight
+
+	db "It habitually digs"
+	next "up and hoards gems"
+	next "in its nest. Its"
+
+	page "loot is constantly"
+	next "targeted by"
+	next "thieves.@"
